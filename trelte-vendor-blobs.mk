419aa6270e04dc71a07ff8c93781c213680dbd3f
Christopher N. Hesse <raymanfx@gmail.com>
2015-02-02 21:42:37 +0100

    trelte: add vcsFPService blob for fingerprint reader capabilities

--- trelte-vendor-blobs.mk
+++ trelte-vendor-blobs.mk
@@ -147,4 +147,5 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/trelte/proprietary/lib/libreference-ril.so:system/lib/libreference-ril.so \
     vendor/samsung/trelte/proprietary/lib/libril.so:system/lib/libril.so \
     vendor/samsung/trelte/proprietary/lib/librilutils.so:system/lib/librilutils.so \
-    vendor/samsung/trelte/proprietary/bin/IPSecService:system/bin/IPSecService
+    vendor/samsung/trelte/proprietary/bin/IPSecService:system/bin/IPSecService \
+    vendor/samsung/trelte/proprietary/bin/vcsFPService:system/bin/vcsFPService
