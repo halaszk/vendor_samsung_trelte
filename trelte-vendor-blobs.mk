1da6844fad2567fae765d0164cfce5270793f179
Christopher N. Hesse <raymanfx@gmail.com>
2015-02-02 21:45:16 +0100

    trelte: add argosd blob for network throughput support

--- trelte-vendor-blobs.mk
+++ trelte-vendor-blobs.mk
@@ -149,4 +149,5 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/trelte/proprietary/lib/librilutils.so:system/lib/librilutils.so \
     vendor/samsung/trelte/proprietary/bin/IPSecService:system/bin/IPSecService \
     vendor/samsung/trelte/proprietary/bin/vcsFPService:system/bin/vcsFPService \
-    vendor/samsung/trelte/proprietary/bin/actlmand:system/bin/actlmand
+    vendor/samsung/trelte/proprietary/bin/actlmand:system/bin/actlmand \
+    vendor/samsung/trelte/proprietary/bin/argosd:system/bin/argosd
